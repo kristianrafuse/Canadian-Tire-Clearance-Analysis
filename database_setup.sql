5d8429a58b96a4b60123035e10c0621e59407116
kristianrafuse <kristianrafuse@gmail.com>
2023-07-26 19:20:26 -0300

    database setup

---
+++ database_setup.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "clearance" (
+	"product_name" varchar,
+	"original_price" float,
+	"clearance_price" float,
+	"percentage_off" float,
+	"product_category" varchar,
+    "product_code" varchar,
+    "rating" integer,
+	"link" varchar
+);
