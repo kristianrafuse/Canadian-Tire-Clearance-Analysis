cf88ff4cb2e308428c9b3aaa4ed55da588990100
kristianrafuse <kristianrafuse@gmail.com>
2023-07-26 20:12:05 -0300

    Complete
    Scraping, cleaning, database setup complete.

--- database_setup.sql
+++ database_setup.sql
@@ -5,6 +5,39 @@ CREATE TABLE "clearance" (
 	"percentage_off" float,
 	"product_category" varchar,
     "product_code" varchar,
-    "rating" integer,
+    "rating" float,
+	"link" varchar
+);
+
+CREATE TABLE "hot_sale" (
+	"product_name" varchar,
+	"original_price" float,
+	"clearance_price" float,
+	"percentage_off" float,
+	"product_category" varchar,
+    "product_code" varchar,
+    "rating" float,
+	"link" varchar
+);
+
+CREATE TABLE "red_alert_deals" (
+	"product_name" varchar,
+	"original_price" float,
+	"clearance_price" float,
+	"percentage_off" float,
+	"product_category" varchar,
+    "product_code" varchar,
+    "rating" float,
+	"link" varchar
+);
+
+CREATE TABLE "sales" (
+	"product_name" varchar,
+	"original_price" float,
+	"clearance_price" float,
+	"percentage_off" float,
+	"product_category" varchar,
+    "product_code" varchar,
+    "rating" float,
 	"link" varchar
 );
