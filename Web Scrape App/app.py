d968afe2798b3270c322aa5d3eb835750f66e45b
kristianrafuse <kristianrafuse@gmail.com>
2023-07-31 13:49:12 -0300

    updates

--- Web Scrape App/app.py
+++ Web Scrape App/app.py
@@ -1,3 +1,20 @@
+import time
+import math 
+import pandas as pd
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from urllib.parse import urlparse
+from sqlalchemy import create_engine, Column, String, Integer, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import re
+
 from flask import Flask, jsonify, render_template, request
 from sqlalchemy import create_engine, Column, String, Integer, Float
 from sqlalchemy.ext.declarative import declarative_base
@@ -20,13 +37,6 @@ engine = create_engine(url)
 
 # Create a base class for declarative models
 Base = declarative_base()
-
-# Define the UserPreferences model for storing email addresses
-class UserPreferences(Base):
-    __tablename__ = 'user_preferences'
-    id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    threshold = Column(Float, nullable=False)
 
 # Create the table in the database
 Base.metadata.create_all(engine)
